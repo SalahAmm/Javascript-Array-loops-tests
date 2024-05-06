81c306551026625fc9552f8b24cfbabc59a36f73
Salah ammour <salahamm4year@gmail.com>
2024-05-06 20:29:31 +0100

    Complete test 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function (Fnum, Lnum) {
+  // create the sum varible
+  let sum = 0;
 
+  // create the loop the sum all the integers between argument
+
+  if ( !isNaN(Fnum) && !isNaN(Lnum)) {
+    if (typeof Fnum !== "string" && typeof Lnum !== "string") {
+      if (Fnum > Lnum) {
+        for (let i = Lnum; i < Fnum + 1; i++) {
+          sum += i;
+        }
+      } else if (Fnum < 0) {
+        return "ERROR";
+      } else {
+        for (let i = Fnum; i < Lnum + 1; i++) {
+          sum += i;
+        }
+      }
+    } else {
+      return "ERROR";
+    }
+  } else {
+    return "ERROR";
+  }
+
+  //return the Sum value
+  return sum;
 };
 
 // Do not edit below this line
