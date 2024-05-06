6867eb7f1b0e69352f299edf145b0307230da12f
Salah ammour <salahamm4year@gmail.com>
2024-05-06 09:34:59 +0100

    Complete javascript test 01-02-03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(name) {
+    const reverse = [...name].reverse().join("");
+    return reverse
 };
 
 // Do not edit below this line
