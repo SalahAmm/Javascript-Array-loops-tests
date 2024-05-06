158974e529c8659d6458ee0ee7b488347b16f18f
Salah ammour <salahamm4year@gmail.com>
2024-05-06 21:49:16 +0100

    Complete test 07 javascript

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fan) {
+  let exoo = (5/9) * (fan - 32);
+  let str = exoo.toFixed(1);
+  let cal = Number(str)
+  return cal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cal) {
+  let exo = (9/5) * cal + 32;
+  let str = exo.toFixed(1);
+  let fan = Number(str);
+  return fan;
 };
 
 // Do not edit below this line
