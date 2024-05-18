31605f40891e22e65709daad42b373d28dab2963
SalahAmm <salahamm4year@gmail.com>
2024-05-18 19:38:56 +0100

    Complete 3 test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count 
+    if ( typeof n !== 'number'){
+            count = parseInt(n);
+    }else {
+        count = n;
+    }
+    if(count == 0) {
+     return 0;
+    }
+    if(count < 0) {
+        return "OOPS";
+    }
 
-};
+    lastNum = 1;
+    firsNum = 0;
+    for (i = 2 ; i <= count ; i++){
+        let current = lastNum + firsNum;
+        firsNum = lastNum;
+        lastNum = current;
+    }
+    return lastNum;
+
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
