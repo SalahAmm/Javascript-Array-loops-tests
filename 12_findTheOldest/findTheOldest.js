5965b667fcc850e341ffbe7c82029233640574cf
SalahAmm <salahamm4year@gmail.com>
2024-05-20 10:16:26 +0100

    Complete all test of javascript

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (person) {
+    return person.reduce((oldNow , currentPerson) => {
+        const oldPer = oldNow.yearOfDeath ? oldNow.yearOfDeath - oldNow.yearOfBirth :new Date().getFullYear() - oldNow.yearOfBirth;
+        const currentPer = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
 
-};
+        if (currentPer > oldPer){
+            return currentPerson;
+        }else {
+            return oldNow;
+        }
+})
+
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
